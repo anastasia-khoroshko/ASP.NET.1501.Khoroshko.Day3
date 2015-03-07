e414ff9cd6f47d7ffea6988fb16efd76ba2a2d26
anastasia-khoroshko <anastasia.khoroshko@gmail.com>
2015-03-07 18:29:18 +0300

    Day3
    Tasks for Day3

--- PolinomClass/Polinom.cs
+++ PolinomClass/Polinom.cs
@@ -8,92 +8,111 @@ namespace PolinomClass
 {
     public class Polinom
     {
-        int degree;
-        float[] arrayFactor;
-        public Polinom(float[] arrayFactor)
+        private float[] factor;
+        public Polinom(params float[] arrayFactor)
         {
-            this.arrayFactor = arrayFactor;
-            if (arrayFactor == null) this.degree = 0;
-            else this.degree = arrayFactor.Length;
+            factor = arrayFactor;
         }
-
-        public float[] ArrayFactor
+        
+        public float this[int degree]
         {
             get
             {
-                return arrayFactor;
+                return factor[degree];
+            }
+            set
+            {
+                factor[degree] = value;
             }
         }
 
         public static Polinom operator +(Polinom polinom1,Polinom polinom2)
         {
-            Polinom result = new Polinom(new float[Math.Max(polinom1.degree, polinom2.degree)]);
-            if(polinom1.degree>=polinom2.degree)
+            Polinom result = new Polinom(new float[Math.Max(polinom1.factor.Length, polinom2.factor.Length)]);
+            if(polinom1.factor.Length>=polinom2.factor.Length)
             {
-                for (int i = 0; i < polinom2.degree; i++)
-                    result.arrayFactor[i] = polinom1.arrayFactor[i] + polinom2.arrayFactor[i];
-                for (int j = polinom2.degree; j < polinom1.degree; j++)
-                    result.arrayFactor[j] = polinom1.arrayFactor[j];
+                for (int i = 0; i < polinom2.factor.Length; i++)
+                    result.factor[i] = polinom1.factor[i] + polinom2.factor[i];
+                for (int j = polinom2.factor.Length; j < polinom1.factor.Length; j++)
+                    result.factor[j] = polinom1.factor[j];
             }
             else
             {
-                for (int i = 0; i < polinom1.degree; i++)
-                    result.arrayFactor[i] = polinom1.arrayFactor[i] + polinom2.arrayFactor[i];
-                for (int i = polinom1.degree + 1; i < polinom2.degree; i++)
-                    result.arrayFactor[i] = polinom2.arrayFactor[i];
+                for (int i = 0; i < polinom1.factor.Length; i++)
+                    result.factor[i] = polinom1.factor[i] + polinom2.factor[i];
+                for (int i = polinom1.factor.Length + 1; i < polinom2.factor.Length; i++)
+                    result.factor[i] = polinom2.factor[i];
             }
             return result;
         }
 
         public static Polinom operator -(Polinom polinom1,Polinom polinom2)
         {
-            Polinom result = new Polinom(new float[Math.Max(polinom1.degree, polinom2.degree)]);
-            if (polinom1.degree >= polinom2.degree)
-            {
-                for (int i = 0; i < polinom2.degree; i++)
-                    result.arrayFactor[i] = polinom1.arrayFactor[i] - polinom2.arrayFactor[i];
-                for (int j = polinom2.degree; j < polinom1.degree; j++)
-                    result.arrayFactor[j] = polinom1.arrayFactor[j];
-            }
-            else
-            {
-                for (int i = 0; i < polinom1.degree; i++)
-                    result.arrayFactor[i] = polinom1.arrayFactor[i] - polinom2.arrayFactor[i];
-                for (int i = polinom1.degree + 1; i < polinom2.degree; i++)
-                    result.arrayFactor[i] =- polinom2.arrayFactor[i];
-            }
-            return result;
+            for (int i = 0; i < polinom2.factor.Length; i++)
+                polinom2.factor[i] *= (-1);
+            return polinom1 + polinom2;
         }
 
         public static Polinom operator *(Polinom polinom1,Polinom polinom2)
         {
-            Polinom result = new Polinom(new float[polinom1.degree+polinom2.degree-1]);
-            for (int i = 0; i < polinom1.degree; i++)
-                for (int j = 0; j < polinom2.degree; j++)
-                    result.arrayFactor[i+j] += polinom1.arrayFactor[i] * polinom2.arrayFactor[j];
+            Polinom result = new Polinom(new float[polinom1.factor.Length + polinom2.factor.Length - 1]);
+            for (int i = 0; i < polinom1.factor.Length; i++)
+                for (int j = 0; j < polinom2.factor.Length; j++)
+                    result.factor[i+j] += polinom1.factor[i] * polinom2.factor[j];
             return result;
         }
 
         public static Polinom operator / (Polinom polinom1,Polinom polinom2)
         {
             Polinom result;
-            float[] dividend = polinom1.arrayFactor;
-            if (polinom1.degree >= polinom2.degree)
+            float[] dividend = polinom1.factor;
+            if (polinom1.factor.Length >= polinom2.factor.Length)
             {
-                if (polinom1.arrayFactor.Last() == 0|| polinom2.arrayFactor.Last()==0) throw new ArithmeticException("Invalid data of polinom");
-                result = new Polinom(new float[polinom1.degree - polinom2.degree + 1]);
-                for (int i = 0; i < result.degree; i++)
+                if (polinom1.factor.Last() == 0|| polinom2.factor.Last()==0) throw new ArithmeticException("Invalid data of polinom");
+                result = new Polinom(new float[polinom1.factor.Length - polinom2.factor.Length + 1]);
+                for (int i = 0; i < result.factor.Length; i++)
                 {
-                    float tempFactor = dividend[dividend.Length - i - 1] / polinom2.arrayFactor.Last();
-                    result.arrayFactor[result.degree - i - 1] = tempFactor;
-                    for (int j = 0; j < polinom2.degree; j++)
+                    float tempFactor = dividend[dividend.Length - i - 1] / polinom2.factor.Last();
+                    result.factor[result.factor.Length - i - 1] = tempFactor;
+                    for (int j = 0; j < polinom2.factor.Length; j++)
                     {
-                        dividend[dividend.Length - i - j - 1] -= tempFactor * polinom2.arrayFactor[polinom2.degree - j - 1];
+                        dividend[dividend.Length - i - j - 1] -= tempFactor * polinom2.factor[polinom2.factor.Length - j - 1];
                     }
                 }
             }
             else result = null;
             return result;
+        }
+
+        public static bool operator==(Polinom polinom1,Polinom polinom2)
+        {
+            if(polinom1.factor.Length==polinom2.factor.Length)
+            {
+                for(int i=0;i<polinom1.factor.Length;i++)
+                    if(polinom1.factor[i]!=polinom2.factor[i]) return false;
+                return true;               
+            }
+            return false;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (this.GetType() != obj.GetType()) return false;
+            return this == obj;
+        }
+        public static bool operator !=(Polinom polinom1, Polinom polinom2)
+        {
+            if ((polinom1 == polinom2)) return false;
+            return true;
+        }
+
+        public override string ToString()
+        {
+            string polinomStr=this.factor.Last()+"*x^"+(this.factor.Length-1);
+            for(int i=this.factor.Length-2;i>=0;i--)
+                polinomStr+="+"+this.factor[i]+"*x^"+i;
+            return polinomStr;
         }
     }
 }
--- TestPolinomClass/TestMethodsPolinom.cs
+++ TestPolinomClass/TestMethodsPolinom.cs
@@ -14,7 +14,7 @@ namespace TestPolinomClass
             Polinom polinom2 = new Polinom(new float[] { 1, 2, 3,4 });
             Polinom result = polinom1 + polinom2;
             Polinom expected = new Polinom(new float[4] { 2.1F, 4.2F, 3, 4 });
-            Assert.ReferenceEquals(result.ArrayFactor,expected.ArrayFactor );
+            Assert.ReferenceEquals(result,expected );
         }
 
         [TestMethod]
@@ -24,7 +24,7 @@ namespace TestPolinomClass
             Polinom polinom2 = new Polinom(new float[] { 1, 2, 3, 4 });
             Polinom result = polinom1-polinom2;
             Polinom expected = new Polinom(new float[5] { 1, 3.2F, 4.1F, 4.3F,4.9F });
-            Assert.ReferenceEquals(result.ArrayFactor, expected.ArrayFactor);
+            Assert.ReferenceEquals(result, expected);
         }
 
         [TestMethod]
@@ -34,7 +34,7 @@ namespace TestPolinomClass
             Polinom polinom2 = new Polinom(new float[] { 1, 2, 3, 4 });
             Polinom result = polinom1 * polinom2;
             Polinom expected = new Polinom(new float[] { 0, 5, 17, 37, 61,60,44,16 });
-            Assert.ReferenceEquals(result.ArrayFactor, expected.ArrayFactor);
+            Assert.ReferenceEquals(result, expected);
         }
 
         [TestMethod]
@@ -46,8 +46,8 @@ namespace TestPolinomClass
             Polinom result = polinom1 / polinom2;
             Polinom expected = new Polinom(new float[] { 0.7159764F, 1.61538458F });
             Polinom modulo = new Polinom(new float[] { 0.126035422F, 0.5165681F, 0.0000000162590226F, 0.0000000293438234F });
-            Assert.ReferenceEquals(result.ArrayFactor, expected.ArrayFactor);
-            Assert.ReferenceEquals(polinom1.ArrayFactor, modulo.ArrayFactor);
+            Assert.ReferenceEquals(result, expected);
+            Assert.ReferenceEquals(polinom1, modulo);
         }
 
         [TestMethod]
@@ -57,8 +57,23 @@ namespace TestPolinomClass
             Polinom polinom1 = new Polinom(new float[] { 1, 4, 4, 0 });
             Polinom polinom2 = new Polinom(new float[] { 1, 1, 1 });
             Polinom result = polinom1 / polinom2;            
-            Assert.ReferenceEquals(result.ArrayFactor, new ArithmeticException() );
+            Assert.ReferenceEquals(result, new ArithmeticException() );
         }
         
+        [TestMethod]
+        public void TestEqualsPolinom()
+        {
+            Polinom polinom1 = new Polinom(new float[] { 1, 4, 4, 0 });
+            Polinom polinom2 = new Polinom(new float[] { 1, 1, 1 });
+            Assert.AreEqual(polinom1.Equals(polinom2), false);
+        }
+        [TestMethod]
+        public void TestStringPolinom()
+        {
+            Polinom polinom1 = new Polinom(new float[] { 1, 4, 4, 0 });
+                polinom1[3] = 2;
+                Assert.AreEqual<string>(polinom1.ToString(), "2*x^3+4*x^2+4*x^1+1*x^0");
+        }
+
     }
 }
